24998fd3ff936817b756faba6919ae136f7bad5b
Erik Sahlmén <erik.sahlmen@gmail.com>
2019-04-26 23:38:17 +0200

    small change2

--- src/components/question.jsx
+++ src/components/question.jsx
@@ -9,7 +9,7 @@ class Question extends Component {
     return (
       <div style={style}>
         <span className="mr-2">
-          <b>Fråga?:</b>
+          <b>Uppgift:</b>
         </span>
         <span>{this.props.questionText} </span>
         <span>
